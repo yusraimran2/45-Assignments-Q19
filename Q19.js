de540ef308c22c459ad2eed212b42efae461d79e
yusraimran2 <yusraimran12@hotmail.com>
2024-11-12 21:20:37 +0500

    Add files via upload

---
+++ Q19.js
@@ -0,0 +1,5 @@
+"use strict";
+// 19:Dinner Guest:Working with one of the programs from exercise You created dinner table guest list,print amessage indicating the number of people you're inviting to dinner.
+let guests = ["Umaima", "Faryal", "Bushra", "Nazia"];
+let numOfGuests = guests.length;
+console.log("You are inviting" + " " + numOfGuests + " people to dinner");
---
+++ Q19.ts
@@ -0,0 +1,5 @@
+// 19:Dinner Guest:Working with one of the programs from exercise You created dinner table guest list,print amessage indicating the number of people you're inviting to dinner.
+
+let guests:string[] = ["Umaima","Faryal", "Bushra", "Nazia"]
+let numOfGuests:number = guests.length
+console.log("You are inviting" + " " + numOfGuests + " people to dinner")
